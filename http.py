e38a7a6bb1499ea9a9fc2d6434d1c049a686073c
DELL <1040517098@qq.com>
2021-05-10 00:27:26 +0800

    dev5-10

---
+++ http.py
@@ -0,0 +1,11 @@
+
+from urllib import request
+import requests
+class HTTP:
+    def get(self,url,return_json=True):
+        r = request.get(url)
+        #restful
+        #json
+        if r.status_code != 200:
+            return {} if return_json else ''
+        return r.json() if return_json else r.txt
